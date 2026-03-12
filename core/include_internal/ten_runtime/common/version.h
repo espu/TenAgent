2f34490d8e0a4454f9f6a3449f9b531d83e5d8cb
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2026-03-12 10:25:31 +0800

    chore: bump version to 0.11.61 (#2103)

--- core/include_internal/ten_runtime/common/version.h
+++ core/include_internal/ten_runtime/common/version.h
@@ -6,4 +6,4 @@
 //
 #pragma once
 
-#define TEN_RUNTIME_VERSION "0.11.60"
+#define TEN_RUNTIME_VERSION "0.11.61"
--- core/src/ten_manager/src/version.rs
+++ core/src/ten_manager/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "0.11.60";
+pub const VERSION: &str = "0.11.61";
